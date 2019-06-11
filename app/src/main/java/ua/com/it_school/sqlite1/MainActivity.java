aa9479e0fac22a94855e52a43abd13f6109c5cea
Viktor Speranskyy <it@osvita.od.ua>
2019-06-11 20:27:44 +0300

    minor syntax updates

--- app/src/main/java/ua/com/it_school/sqlite1/MainActivity.java
+++ app/src/main/java/ua/com/it_school/sqlite1/MainActivity.java
@@ -6,16 +6,11 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -66,6 +61,8 @@ public class MainActivity extends Activity {
             long rowID = db.insert("students", null, cv);
             db.setTransactionSuccessful();
             Log.d(LOG_TAG, "Добавлена 1 запись с ID = " + rowID);
+        } catch (Exception exc) {
+            Log.d(LOG_TAG, "Unsuccessful insert!");
         } finally {
             db.endTransaction();
         }
@@ -158,8 +155,14 @@ public class MainActivity extends Activity {
         db = dbHelper.getWritableDatabase(); // подключаемся к БД
 
         // удаляем по id
-        int delCount = db.delete("students", "id = " + id, null);
-        Log.d(LOG_TAG, "Всего записей удалено: " + delCount);
+        db.beginTransaction();
+        try {
+            int delCount = db.delete("students", "id = " + id, null);
+            db.setTransactionSuccessful();
+            Log.d(LOG_TAG, "Всего записей удалено: " + delCount);
+        } finally {
+            db.endTransaction();
+        }
 
         // закрываем подключение к БД
         dbHelper.close();
@@ -180,10 +183,10 @@ public class MainActivity extends Activity {
         String queryParam = "%" + editSearch.getText().toString() + "%";
 
         Log.d(LOG_TAG, "Записи, в которых поле Имя или Адрес содержит: " + queryParam);
-        selection = " name LIKE ? or address LIKE ?";
+        selection = " name LIKE ? OR address LIKE ?";
         selectionArgs = new String[]{queryParam, queryParam};
 
-        c = db.query("students", null, selection, selectionArgs, null, null, null);
+        c = db.query("students", new String[]{"id", "name", "address"}, selection, selectionArgs, null, null, "address");
 
         // обработка результатов запроса
         if (c != null) {
